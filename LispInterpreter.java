dfe8ead4942c8da37a7bdc91db9df175b6903aee
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2024-02-28 10:53:17 -0600

    Suma

--- LispInterpreter.java
+++ LispInterpreter.java
@@ -1,3 +1,74 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LispInterpreter {
+    private static LispInterpreter interpreter;
+    private Map<String, Object> globalEnv = new HashMap<>();
+    
+    private LispInterpreter() {
+        globalEnv.put("+", null);
+    }
+    public static LispInterpreter getInterpreter() {
+        if (interpreter == null) {
+            interpreter = new LispInterpreter();
+        }
+        return interpreter;
+    }
+
+
+    public Object eval(List<?> list) throws Exception {
+        String first = (String) list.get(0);
+        switch (first) {
+            case "+":
+                return sum(list.subList(1, list.size()));
+
+            default:
+                Object item = globalEnv.get(first);
+                if (item.getClass() == LispFunction.class) {
+                    if (list.size() - 1 == ((LispFunction) item).getParams().size()) {
+                        return ((LispFunction) item).apply();
+                    } else {
+                        throw new Exception();
+                    }
+                } else {
+                    return item;
+                }
+        }
+
+    }
+
+    public Object sum(List<?> args) throws Exception {
+        Object result = 0; boolean stillInt = true;
+        for (Object o : args) {
+            Object tempResult;
+            if (List.class.isAssignableFrom(o.getClass())) {
+                tempResult = eval((List<?>) o );
+            } else {
+                tempResult = o;
+            }
+            if (tempResult.getClass() == Integer.class) {
+                if (stillInt) {
+                    result = (Integer) result + (Integer) tempResult;
+                } else {
+                    result = (Double) result + (Integer) tempResult;
+                }
+                
+            } else {
+                if (stillInt) {
+                    result = (Integer) result + (Double) tempResult;
+                    stillInt = false;
+                } else {
+                    result = (Double) result + (Double) tempResult;
+                }
+            }
+            
+
+        }
+        return result;
+    }
+
+    
+
     
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        List<Object> l = List.of("+", -0.5, 12.132412412431234, 2, List.of("+", 2, 3), 3.0, 3);
+
+        LispInterpreter lispInterpreter = LispInterpreter.getInterpreter();
+        
+        System.out.println(lispInterpreter.eval(l));
+        
         
     }
 }
