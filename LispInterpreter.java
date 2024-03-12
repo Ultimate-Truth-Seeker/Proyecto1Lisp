41d44abd0c8ffe96e6495a6010f557ea7cbbdba2
Estuardo Castro <cas23890@uvg.edu.gt>
2024-03-12 08:33:05 -0600

    Funciones aritméticas

--- LispInterpreter.java
+++ LispInterpreter.java
@@ -277,15 +277,46 @@ public class LispInterpreter {
     //TODO: implement methods
 
     public Object substract(List<?> args) throws Exception {
-        return null;
+        if (args.isEmpty()) {
+            throw new Exception("No arguments for subtraction");
+        }
+    
+        double result = (Double) args.get(0);
+    
+        for (int i = 1; i < args.size(); i++) {
+            result -= (Double) args.get(i);
+        }
+    
+        return result;
     }
 
     public Object product(List<?> args) throws Exception {
-        return null;
+        if (args.isEmpty()) {
+            throw new Exception("No arguments for multiplication");
+        }
+    
+        double result = 1.0;
+
+        for (Object arg : args) {
+            result *= (Double) arg;
+        }
+    
+        return result;
     }
 
     public Object divide(List<?> args) throws Exception {
-        return null;
+        if (args.size() != 2) {
+        throw new Exception("Expected exactly two arguments for division");
+        }
+
+        double numerator = (Double) args.get(0);
+        double denominator = (Double) args.get(1);
+
+        if (denominator == 0) {
+        throw new Exception("Cannot divide by zero");
+        }
+
+        return numerator / denominator;
     }
 
     // La función 'moreThan' toma una lista de dos argumentos y devuelve verdadero si el primer argumento es mayor que el segundo.
